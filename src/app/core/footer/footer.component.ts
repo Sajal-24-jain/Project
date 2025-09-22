de2c12fb16c51f0488102ecfdbea2ad081888645
Sajal-24-jain <sajaljain780@gmail.com>
2025-09-23 01:56:19 +0530

    Updated Footer

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,11 +1,24 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-footer',
+//   imports: [],
+//   templateUrl: './footer.component.html',
+//   styleUrl: './footer.component.scss'
+// })
+// export class FooterComponent {
+
+// }
+
+
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
-
-}
+export class FooterComponent { }
