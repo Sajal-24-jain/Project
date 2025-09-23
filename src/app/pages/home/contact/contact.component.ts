4962617bd9c0a69ff4995448516fc0bf1a7a0636
Sajal-24-jain <sajaljain780@gmail.com>
2025-09-23 23:58:59 +0530

    contact

--- src/app/pages/home/contact/contact.component.ts
+++ src/app/pages/home/contact/contact.component.ts
@@ -1,11 +1,39 @@
+// import { Component } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { FormsModule } from '@angular/forms';
+
+// @Component({
+//   selector: 'app-contact',
+//   standalone: true,
+//   imports: [CommonModule, FormsModule],
+//   templateUrl: './contact.component.html',
+//   styleUrls: ['./contact.component.scss']
+// })
+// export class ContactComponent {
+//   contact = {
+//     name: '',
+//     email: '',
+//     message: ''
+//   };
+
+//   submitForm() {
+//     if (this.contact.name && this.contact.email && this.contact.message) {
+//       alert('Thank you for contacting us!');
+//       this.contact = { name: '', email: '', message: '' };
+//     } else {
+//       alert('Please fill in all fields.');
+//     }
+//   }
+// }
+
+
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';  
 
 @Component({
   selector: 'app-contact',
+  imports: [FormsModule],
   standalone: true,
-  imports: [CommonModule, FormsModule],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
@@ -13,15 +41,14 @@ export class ContactComponent {
   contact = {
     name: '',
     email: '',
+    subject: '',
     message: ''
   };
 
   submitForm() {
-    if (this.contact.name && this.contact.email && this.contact.message) {
-      alert('Thank you for contacting us!');
-      this.contact = { name: '', email: '', message: '' };
-    } else {
-      alert('Please fill in all fields.');
-    }
+    console.log('Form submitted:', this.contact);
+    // Here you can integrate API call to send the form
+    alert('Thank you for reaching out! We’ll get back to you soon.');
+    this.contact = { name: '', email: '', subject: '', message: '' };
   }
 }
